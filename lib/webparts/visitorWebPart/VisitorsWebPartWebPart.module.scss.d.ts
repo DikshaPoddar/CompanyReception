587517df6dd4b81a2440fba8693c9f3f2a9cdd43
DikshaPoddar <50654395+DikshaPoddar@users.noreply.github.com>
2019-05-16 00:14:04 +0530

    Add files via upload

---
+++ lib/webparts/visitorWebPart/VisitorsWebPartWebPart.module.scss.d.ts
@@ -0,0 +1,16 @@
+declare const styles: {
+    visitorsWebPart: string;
+    container: string;
+    row: string;
+    column: string;
+    'ms-Grid': string;
+    title: string;
+    subTitle: string;
+    description: string;
+    button: string;
+    label: string;
+    Vtable: string;
+    dataTable: string;
+    required: string;
+};
+export default styles;
